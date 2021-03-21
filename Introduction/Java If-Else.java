3f997a68d980ffe2ea98622e8fbb7a4db5eb9482
venky9694 <54945968+venky9694@users.noreply.github.com>
2021-03-21 16:21:33 +0530

    Create Java If-Else.java

---
+++ Introduction/Java If-Else.java
@@ -0,0 +1,57 @@
+/*
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+*/
+
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String ans = "";
+    if (n%2 == 0 && (n>20 || n < 6) )
+    { ans += "Not ";
+    
+    }
+    
+        ans += "Weird";
+        System.out.println(ans);
+        scanner.close();
+    }
+}
