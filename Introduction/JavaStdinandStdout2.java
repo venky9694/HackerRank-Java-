ba2ea0dd16c012110adada6532a73c2b4ac23a5b
venky9694 <54945968+venky9694@users.noreply.github.com>
2021-03-21 16:23:26 +0530

    Create JavaStdinandStdout2.java

---
+++ Introduction/JavaStdinandStdout2.java
@@ -0,0 +1,48 @@
+/*
+Input Format
+
+There are three lines of input:
+
+The first line contains an integer.
+The second line contains a double.
+The third line contains a String.
+Output Format
+
+There are three lines of output:
+
+On the first line, print String: followed by the unaltered String read from stdin.
+On the second line, print Double: followed by the unaltered double read from stdin.
+On the third line, print Int: followed by the unaltered integer read from stdin.
+
+Sample Input
+
+42
+3.1415
+Welcome to HackerRank's Java tutorials!
+Sample Output
+
+String: Welcome to HackerRank's Java tutorials!
+Double: 3.1415
+Int: 42
+
+*/
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt();
+        
+        double d=scan.nextDouble();
+        scan.nextLine();
+        String s=scan.nextLine();
+       
+       
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
