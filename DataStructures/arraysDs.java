e893d08b42882f23e4966730bc6a2d6371a26d00
venky9694 <54945968+venky9694@users.noreply.github.com>
2021-09-01 16:42:35 +0530

    Create arraysDs.java

---
+++ DataStructures/arraysDs.java
@@ -0,0 +1,116 @@
+/*
+@Author :Venkatesh Kumawat
+
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  or .
+
+Reverse an array of integers.
+
+Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
+
+Example
+
+Return .
+
+Function Description
+
+Complete the function reverseArray in the editor below.
+
+reverseArray has the following parameter(s):
+
+int A[n]: the array to reverse
+Returns
+
+int[n]: the reversed array
+Input Format
+
+The first line contains an integer, , the number of integers in .
+The second line contains  space-separated integers that make up .
+
+Constraints
+
+Sample Input 1
+
+CopyDownload
+Array: arr
+1
+4
+3
+2
+
+ 
+4
+1 4 3 2
+Sample Output 1
+
+2 3 4 1
+
+Solutions provided below
+
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+    // Write your code here
+    //using for loop for the reverse
+    
+    List<Integer>reverse=new ArrayList<>(a.size());
+    //starting of for loop
+    for(int i=a.size()-1;i>=0;i--)
+    {
+        reverse.add(a.get(i));
+    }
+    
+    return reverse;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> res = Result.reverseArray(arr);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
+
+
+
+
